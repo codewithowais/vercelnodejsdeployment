7d80738a9d08ef95fb0dab8fce6cd5701824f1bc
BasitAyaz <basitahmed1997@gmail.com>
2024-07-28 17:35:41 +0500

    setup

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const {
   createUser,
   updateUser,
 } = require("./controllers/userController");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const app = express(); //
 const port = 3000;
 
-mongoose
-  .connect(
-    "mongodb+srv://tideto4179:nPdphum5IzkkkSPz@cluster0.b7regnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then((res) => console.log(`Database connected successfuly!.....`))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(
+//     "mongodb+srv://tideto4179:nPdphum5IzkkkSPz@cluster0.b7regnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//   )
+//   .then((res) => console.log(`Database connected successfuly!.....`))
+//   .catch((err) => console.log(err));
 
 const data = [];
 
