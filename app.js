508d36931b9d1fbcf0338a509f038633e92613d3
BasitAyaz <basitahmed1997@gmail.com>
2024-07-28 17:14:45 +0500

    setup

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express"); // import express js
+const {
+  getUsers,
+  createUser,
+  updateUser,
+} = require("./controllers/userController");
+const mongoose = require("mongoose");
+const app = express(); //
+const port = 3000;
+
+mongoose
+  .connect(
+    "mongodb+srv://tideto4179:nPdphum5IzkkkSPz@cluster0.b7regnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then((res) => console.log(`Database connected successfuly!.....`))
+  .catch((err) => console.log(err));
+
+const data = [];
+
+app.get("/", getUsers);
+app.post("/create", createUser);
+app.put("/:id", updateUser);
+app.delete("/:id", createUser);
+
+// app.get("/", (req, res) => {
+//   res.send(data);
+// });
+
+app.get("/users", (req, res) => {
+  res.send("Hello welcome to user api");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port} port;`);
+});
